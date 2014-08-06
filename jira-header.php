f65f0bc6c7e79b53b2dc243de0b52b7272ab04d4
Andrew Roslik <andkirby@gmail.com>
2014-08-06 20:25:05 +0300

    - Added showing non-code issues.  - Added skipping build notes issue.

--- jira-header.php
+++ jira-header.php
@@ -2,7 +2,7 @@
 $inProgress = $requiredFixVersionInProgress ? 'YES' : 'NO';
 $activeSprintIdString = implode(', ', $activeSprintIds);
 echo <<<HEADER
-JiGIT - JIRA GIT Synchronization Tool v0.1.2
+JiGIT - JIRA GIT Synchronization Tool v0.2.0
 https://github.com/andkirby/jigit-sync
 =================================================
 Project:             $project
