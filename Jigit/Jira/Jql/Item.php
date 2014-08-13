c7bff4ee506e1a6435bad2c718eeb23071b53159
Andrew Roslik <andkirby@gmail.com>
2014-08-13 11:41:07 +0300

    - Added empty JQL item class and interface.

---
+++ Jigit/Jira/Jql/Item.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kirby
+ * Date: 13.08.2014
+ * Time: 8:46
+ */
+
+namespace Jigit\Jira\Jql;
+
+/**
+ * Class Item
+ *
+ * @package Jigit\Jira\Jql
+ */
+class Item implements ItemInterface
+{
+} 
---
+++ Jigit/Jira/Jql/ItemInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kirby
+ * Date: 13.08.2014
+ * Time: 8:46
+ */
+
+namespace Jigit\Jira\Jql;
+
+/**
+ * Interface ItemInterface
+ *
+ * @package Jigit\Jira\Jql
+ */
+interface ItemInterface
+{
+} 
