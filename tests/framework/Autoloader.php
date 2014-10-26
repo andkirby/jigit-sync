a423517aef85d3a3e732cbdcba3115e49db011fd
Andrew Roslik <andkirby@gmail.com>
2014-10-27 01:16:02 +0200

    - Added base tests files.

---
+++ tests/framework/Autoloader.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Class for autoload classes
+ */
+class Autoloader
+{
+    /**
+     * Exception loader code
+     */
+    const EXCEPTION_CODE = 564;
+
+    /**
+     * Aliases list
+     *
+     * @var array
+     */
+    protected static $_aliases = array();
+
+    /**
+     * Load class
+     *
+     * @param $class
+     * @throws Exception
+     */
+    static public function autoload($class)
+    {
+        if (strpos($class, '_')) {
+            $class = str_replace('_', '/', $class);
+        }
+        $file = str_replace('\\', '/', $class) . '.php';
+        $file = self::_applyAliases($file);
+        if (self::_isExist($file)) {
+            include_once $file; ;
+        } else {
+            throw new \Exception(
+                'Could not load file ' . $file . ' in include path: ' .
+                get_include_path(), self::EXCEPTION_CODE
+            );
+        }
+    }
+
+    /**
+     * Check does class file
+     *
+     * @param string $file
+     * @return bool
+     */
+    static protected function _isExist($file)
+    {
+        foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
+            $path = str_replace('\\', '/', rtrim($path, '\\/'));
+            if (is_file($path . '/' . $file)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Register autoloader
+     */
+    static public function register()
+    {
+        spl_autoload_register(__NAMESPACE__ . '\Autoloader::autoload');
+    }
+
+    /**
+     * Folder load alias
+     *
+     * @param string $name
+     * @param string $alias
+     */
+    public static function addAlias($name, $alias)
+    {
+        /**
+         * Add "|" to make search from start
+         */
+        self::$_aliases['|' . $name] = $alias;
+    }
+
+    /**
+     * Apply aliases
+     *
+     * @param string $file
+     * @return string
+     */
+    protected static function _applyAliases($file)
+    {
+        $fileNew = str_replace(array_keys(self::$_aliases), self::$_aliases, '|' . $file);
+        if ('|' !== $fileNew[0]) {
+            //return changed path to file
+            $file = $fileNew;
+        }
+        return $file;
+    }
+}
---
+++ tests/framework/bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+@include_once 'func.php';
+
+//try to load vendor/autoload.php
+@include_once 'autoload.php';
+
+$includePaths = array(
+    get_include_path(),
+    realpath(__DIR__ . '/..') . '/framework',
+    realpath(__DIR__ . '/..') . '/testsuite',
+    realpath(__DIR__ . '/../..') . '/lib',
+    realpath(__DIR__ . '/../../..') . '/jira-api-restclient',
+);
+define('PROJECT_ROOT', realpath(__DIR__ . '/../..'));
+set_include_path(implode(PATH_SEPARATOR, $includePaths));
+
+
+require_once 'Autoloader.php';
+\Autoloader::register();
+
+/**
+ * Add alias for class loading because chobie directory does not exist
+ */
+\Autoloader::addAlias('chobie/Jira', 'src/Jira');
+
+//set default PHPUnit error handler
+set_error_handler('\PHPUnit_Util_ErrorHandler::handleError');
+
