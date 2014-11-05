47462f9ae164fd68eb8dfa775c93c8f40fc11b73
Andrew Roslik <andkirby@gmail.com>
2014-11-05 11:59:21 +0200

    - Updated process installation

--- jigit-install.sh
+++ jigit-install.sh
@@ -2,8 +2,8 @@
 git clone https://github.com/chobie/jira-api-restclient.git
 git clone https://github.com/andkirby/jigit-sync.git
 cd jigit-sync
-echo "yourpassword" >> jira.password
-vim jira.password
-cp jira-common.php.dist jira-common.php
-vim jira-common.php
-php jira.php
+echo "yourpassword" >> config/jira.password
+vim config/jira.password
+cp config/local.yml.dist config/local.yml
+vim config/local.yml
+php jira.php h
