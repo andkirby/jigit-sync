8090bbd29e2c60ae9418661587c9979f0be36480
Andrew Roslik <andkirby@gmail.com>
2014-11-07 14:37:02 +0200

    - Updated install. - Removed old file.

--- jigit-install.sh
+++ jigit-install.sh
@@ -1,9 +1,53 @@
 #!/bin/sh
-git clone https://github.com/chobie/jira-api-restclient.git
-git clone https://github.com/andkirby/jigit-sync.git
+echo "Fetching files from GitHub..."
+
+OUTPUT=$(git clone https://github.com/chobie/jira-api-restclient.git 2>&1)
+if echo "$OUTPUT" | grep -qE "error\:|fatal\:"; then
+    echo "Ooops.. Something wrong."
+    echo $OUTPUT
+    exit 1
+fi
+echo $OUTPUT
+OUTPUT=$(git clone https://github.com/andkirby/jigit-sync.git 2>&1)
+if echo "$OUTPUT" | grep -qE "error\:|fatal\:"; then
+    echo "Ooops.. Something wrong."
+    echo $OUTPUT
+    exit 1
+fi
+echo $OUTPUT
+
 cd jigit-sync
-echo "yourpassword" >> config/jira.password
-vim config/jira.password
-cp config/local.yml.dist config/local.yml
-vim config/local.yml
-php jira.php h
+
+echo "
+Getting into jigit-sync directory..."
+
+echo "Please set your JIRA credentials and JIRA URL (your password will be encrypted).""
+
+while [ "$USERNAME" -eq "" ] ; do
+    #read username
+    printf "JIRA username: "
+    read USERNAME
+done
+while [ "$PASSWORD" -eq "" ] ; do
+    #read password
+    printf "JIRA password: "
+    read -s PASSWORD
+done
+while [ "$JIRA_URL" -eq "" ] ; do
+    #read JIRA URL
+    printf "JIRA URL (e.g. http://jira.example.com): "
+    read JIRA_URL
+done
+
+echo $PASSWORD >> config/jira.password
+
+echo "
+app:
+  jira:
+    username: $USERNAME
+    url: $JIRA_URL" >> config/local.yml
+
+echo "Please take a look JiGIT Tool manual by commands:"
+echo "
+    cd jigit-sync
+    php jira.php h"
