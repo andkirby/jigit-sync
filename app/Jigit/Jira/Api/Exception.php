b47aa1a5040a4f1d0602dde8a6796f6c99baeca6
Andrew Roslik <andkirby@gmail.com>
2014-11-11 13:21:33 +0200

    - Added API exception class.

---
+++ app/Jigit/Jira/Api/Exception.php
@@ -0,0 +1,13 @@
+<?php
+namespace Jigit\Jira\Api;
+
+use chobie\Jira as JiraLib;
+
+/**
+ * Class JIRA API Exception
+ *
+ * @package Jigit\Jira\Api
+ */
+class Exception extends JiraLib\Api\Exception
+{
+}
