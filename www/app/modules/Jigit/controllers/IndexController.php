06c2c7a224114fcff7d6a8b8780cc25fa3283a21
Andrew Roslik <andkirby@gmail.com>
2014-11-15 15:30:31 +0200

    - Added initializing required models.

--- www/app/modules/Jigit/controllers/IndexController.php
+++ www/app/modules/Jigit/controllers/IndexController.php
@@ -8,6 +8,19 @@ use Lib\Controller;
 class IndexController extends Controller\AbstractController
 {
     /**
+     * Initialize
+     */
+    public function init()
+    {
+        $run = new Run();
+        $run->initConfig();
+        \Zend_Registry::set('vcs', new Git());
+
+        //TODO Refactor this set
+        !defined('JIGIT_ROOT') && define('JIGIT_ROOT', realpath(APP_ROOT . '/..'));
+    }
+
+    /**
      * Index action
      */
     public function indexAction()
