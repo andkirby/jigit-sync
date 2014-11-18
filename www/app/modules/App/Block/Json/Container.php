beb404f9a42431fe1eabe926ed4987542c85e5b5
Andrew Roslik <andkirby@gmail.com>
2014-11-18 02:16:44 +0200

    - Implemented JSON container block.

---
+++ www/app/modules/App/Block/Json/Container.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Block\Json;
+
+use App\Block\Json;
+
+/**
+ * Class Container
+ *
+ * Get JSON data from child blocks
+ *
+ * @package App\Block
+ */
+class Container extends Json
+{
+    /**
+     * Get child JSON data
+     *
+     * @return string
+     */
+    public function getDataToJson()
+    {
+        $data = array();
+        foreach ($this->getChildren() as $name => $block) {
+            if ($block instanceof Json) {
+                $data[$name] = array('data' => $block->getDataToJson());
+            } else {
+                $data[$name] = array('html' => $block->toHtml());
+            }
+        }
+        return $data;
+    }
+}
